841c2b8d4a6dc4fd91ff451133a9116b3b07c47f
Guilherme <oliveira.guiribeiro.guilherme@gmail.com>
2024-06-03 21:35:25 +0100

    por atualizar o resto do código

--- Utilizador/routes.py
+++ Utilizador/routes.py
@@ -1,7 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint,jsonify, request,make_response
+from models import db, Utilizador
+from flask_login import login_user, current_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 utilizador_blueprint = Blueprint('utilizador_api_routes', __name__, url_prefix='/api/utilizador')
 
-@utilizador_blueprint.route('/')
+@utilizador_blueprint.route('/criar', methods=['POST'])
+def criar_utilizador():
+    try:
+        # Verificação se o nomeUtilizador e apassword estao presentes
+        nomeUtilizador = request.form.get
 def index():
     return 'Olá Turma'
